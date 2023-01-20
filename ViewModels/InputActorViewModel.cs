01c620e57632ebc77ad3489f2547780559f54073
Redrick Clicks <den-draks@yandex.ru>
2023-01-21 01:34:37 +0300

    added an actor model

---
+++ ViewModels/InputActorViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MoviesApp.ViewModels
+{
+
+    public class InputActorViewModel
+    {
+        //public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        [DataType(DataType.Date)] 
+        public DateTime BirthDate { get; set; }
+    }
+}

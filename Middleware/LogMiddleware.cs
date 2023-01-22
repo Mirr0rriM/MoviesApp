5c8a3763d3313b59ac528ced42737443ad455a9e
Redrick Clicks <den-draks@yandex.ru>
2023-01-22 22:33:20 +0300

    add middleware

---
+++ Middleware/LogMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace MoviesApp.Middleware
+{
+    public class LogMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public LogMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext, ILogger<LogMiddleware> logger)
+        {
+            if (httpContext.Request.Path.StartsWithSegments("/Actors"))
+            {
+                logger.LogTrace($"Request: {httpContext.Request.Path}  Method: {httpContext.Request.Method}");
+                if (httpContext.Request.Method == HttpMethod.Post.Method && httpContext.Request.Form != null)
+                {
+                    foreach (var item in httpContext.Request.Form)
+                    {
+                        logger.LogTrace($"Key: {item.Key}  Value: {item.Value}");
+                    }
+                }
+            }
+            await _next(httpContext);
+        }
+    }
+}
---
+++ Middleware/LogMiddlewareExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace MoviesApp.Middleware
+{
+    public static class LogMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseRequestLog(
+            this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<LogMiddleware>();
+        }
+    }
+}

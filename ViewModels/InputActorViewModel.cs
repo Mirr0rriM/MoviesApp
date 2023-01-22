0c9716a9590430a70875065a918d9a9a3e8ad798
Redrick Clicks <den-draks@yandex.ru>
2023-01-23 00:10:35 +0300

    add validation of name and surname

--- ViewModels/InputActorViewModel.cs
+++ ViewModels/InputActorViewModel.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using MoviesApp.Filters;
 
 namespace MoviesApp.ViewModels
 {
 
     public class InputActorViewModel
     {
-        //public int Id { get; set; }
+        [NameValidation(4)]
         public string Name { get; set; }
+        
+        [NameValidation(4)]
         public string Surname { get; set; }
 
         [DataType(DataType.Date)] 
